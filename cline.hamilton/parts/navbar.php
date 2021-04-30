8dbdda9da0fb2d7c9177d448963f862efb37b95b
Hamilton <hamdiggy@gmail.com>
2021-04-29 18:51:36 -0700

    module 11

--- cline.hamilton/parts/navbar.php
+++ cline.hamilton/parts/navbar.php
@@ -8,7 +8,10 @@
          <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="product_list.php">Products</a></li>
-            <li><a href="product_cart.php">Cart</a></li>
+            <li><a href="product_cart.php">
+               <span>Cart</span>
+               <span class="badge"><?= makeCartBadge() ?></span>
+            </a></li>
          </ul>
       </nav>
    </div>
--- cline.hamilton/product_added_to_cart.php
+++ cline.hamilton/product_added_to_cart.php
@@ -2,6 +2,13 @@
 
 include "lib/php/functions.php";
 
+// pretty_dump($_POST);
+
+$product = MYSQLIQuery("SELECT * FROM `products` WHERE `id`=".$_GET['id'])[0];
+$cart_product = cartItemById($product->id);
+
+//pretty_dump($product);
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,13 +22,22 @@ include "lib/php/functions.php";
 
    <div class="container">
       <div class="card soft">
-         <h2>#### Added To Your Cart</h2>
+         <?php
 
-         <div class="display-flex">
-            <div class="flex-none"><a class="form-button" href="javascript:window.history.back();">Back To Product</a></div>
-            <div class="flex-stretch"></div>
-            <div class="flex-none"><a class="form-button" href="product_list.php">Continue Shopping</a></div>
-         </div>
+         if(!isset($_GET['id'])) {
+            echo "You dun goofed";
+         } else {
+            ?>
+            <h2><?= $cart_product->amount ?> <?= $product->title ?> In Your Cart</h2>
+
+            <div class="display-flex">
+               <div class="flex-none"><a class="form-button" href="javascript:window.history.back();">Back To Product</a></div>
+               <div class="flex-stretch"></div>
+               <div class="flex-none"><a class="form-button" href="product_list.php">Continue Shopping</a></div>
+            </div>
+            <?
+         }
+         ?>
       </div>
    </div>
 </body>
