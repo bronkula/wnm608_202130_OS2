1c11ea51126af529b075def02de59f5f1fc8333f
Hamilton <hamdiggy@gmail.com>
2021-05-20 17:19:17 -0700

    module 14

--- cline.hamilton/parts/navbar.php
+++ cline.hamilton/parts/navbar.php
@@ -1,9 +1,13 @@
+<input type="checkbox" class="hidden" id="nav-toggle">
 <header class="navbar">
    <div class="container display-flex flex-align-center">
       <div class="flex-none">
          <h1>Store Website</h1>
       </div>
       <div class="flex-stretch"></div>
+      <div class="flex-none nav-toggle">
+         <label for="nav-toggle">&equiv;</label>
+      </div>
       <nav class="flex-none nav flex">
          <ul>
             <li><a href="index.php">Home</a></li>
